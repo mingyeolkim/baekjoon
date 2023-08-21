156d72acbb0594eec886edf6c8a8af3351e5cddc
mingyeolkim <129843693+mingyeolkim@users.noreply.github.com>
2023-08-21 16:52:04 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = []
+cnt = 0
+for _ in range(n):
+    a = input()
+    arr.append(a)
+for i in arr:
+    for j in range(0, len(i) - 1):
+        if i[j] == i[j+1]:
+            pass
+        elif i[j] in i[j+1:]:
+            cnt += 1
+            break
+print(n - cnt)
